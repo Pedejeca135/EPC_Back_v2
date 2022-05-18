0c3161b9bd2efaa17ff4b3b70c2a125c18fc6f37
pedejeca135 <pjco90@hotmail.com>
2022-05-18 03:27:57 -0500

    primera validacion con sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class,'login']);
+
+Route::group(['middleware' => ["auth:sanctum"]], function(){
+    Route::get('user-profile', [UserController::class,'userProfile']);
+    Route::get('logout', [UserController::class,'logout']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
